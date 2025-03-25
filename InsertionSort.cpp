02728eaf96841e8ce06f6da817930454460b266e
NatasyaAlia <natasyaalia436@gmail.com>
2025-03-25 22:53:38 +0700

    Membuat Variable data temporer atau penyimpanan sementara

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -17,7 +17,26 @@ break;
 }
 else
 {
- cout << "/nArray yang masukkan maksimal 20 Elemen./n"; //Menampilkan Pesan jika data lebih dari 20   
+cout << "\nArray yang masukkan maksimal 20 Elemen.\n"; //Menampilkan Pesan jika data lebih dari 20   
+}
+cout << "\nArray yang anda masukkan maksimal 20 Elemen.\n"; // Menampilkan Pesan jika data lebih dari 20
 }
 }
+cout << endl; // Membuat jarak per baris program
+cout << "===================" << endl; //Membuat tampilan susunan data element array
+cout << "Masukkan Element Array" << endl;
+cout << "====================" << endl;
+
+for (int i = 0; i n; i++) // Menggunakan perulangan for untuk menyimpan data pada array
+{
+    cout << "Data ke-" << (i + 1) << ": "; //Masukkan atau mengimputkan nilai data n
+    cin >> arr [i]; // Menyimpan nilai data n kedalam array arr
+}
+
+void InsertionSort()
+{ // Procedure InsertionSort
+
+    int temp; // Membuat variable data temporer atau penyimpanan sementara
+    int j. i; // Membuat variable j sebagai penanda
+
 }
